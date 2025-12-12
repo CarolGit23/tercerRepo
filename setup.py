58af776f5909aff5bb079991376ce9acb2f50b56
Carol <carolina.olaya@tesigroup.tech>
2025-12-12 00:03:59 -0600

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Carolina Olaya",                            # Tu nombre
+    author_email="carolina.olaya@tesigroup.tech",   # Tu correo electrónico
+    url="https://github.com/CarolGit23/tercerrepo", # URL del proyecto
+)
